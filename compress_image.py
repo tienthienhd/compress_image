d588f794696420593dcb0e3572ee9ce71eafeb0e
thiennt <tienthienhd@gmail.com>
2020-11-10 11:44:36 +0700

    done api

--- compress_image.py
+++ compress_image.py
@@ -1,4 +1,6 @@
+import functools
 import os
+import time
 from io import BytesIO
 from typing import List
 from PIL import Image
@@ -9,36 +11,70 @@ FILE_SIZE_THRESHOLD = 500000
 BASE_FOLDER = "data/"
 
 
-def compress_images(imgs: List) -> List[str]:
+def timeit(f):
+    @functools.wraps(f)
+    def run(*args, **kwargs):
+        start_time = time.time()
+        result = f(*args, **kwargs)
+        end_time = time.time()
+        print("Time exec '{}': {:.3}s".format(f.__name__, end_time - start_time))
+        return result
+
+    return run
+
+
+@timeit
+def compress_images(imgs: List, byte_images: List[int]) -> List[str]:
     file_names = []
-    for img in imgs:
+    for img, byte_image in zip(imgs, byte_images):
         file_name = None
         try:
-            file_name = compress_image(img)
+            file_name = compress_image(img, byte_image)
         except:
             pass
         file_names.append(file_name)
     return file_names
 
 
-def compress_image(img) -> str:
-    hash = imagehash.dhash(img, hash_size=8)
-    filename = "{}.jpg".format(hash)
-    file_path = os.path.join(BASE_FOLDER, filename)
-    if os.path.exists(file_path):
-        return filename
+# @timeit
+# def compress_image(img) -> str:
+#     hash = imagehash.dhash(img, hash_size=8)
+#     filename = "{}.jpg".format(hash)
+#     file_path = os.path.join(BASE_FOLDER, filename)
+#     if os.path.exists(file_path):
+#         return filename
+#
+#     w, h = img.size
+#     max_dim = max(w, h)
+#     if max_dim > BASE_DIM:
+#         scale = BASE_DIM / max_dim
+#         new_w, new_h = int(scale * w), int(scale * h)
+#         img = img.resize((new_w, new_h), Image.ANTIALIAS)
+#     img_file = BytesIO()
+#     img.save(img_file, 'png')
+#     img_file_size = img_file.tell()
+#     if img_file_size > FILE_SIZE_THRESHOLD:
+#         img.save(file_path, optimize=True, quality=95)
+#     else:
+#         img.save(file_path, optimize=False, quality=100)
+#     return filename
 
+@timeit
+def compress_image(img, byte_image: int) -> BytesIO:
     w, h = img.size
     max_dim = max(w, h)
     if max_dim > BASE_DIM:
         scale = BASE_DIM / max_dim
         new_w, new_h = int(scale * w), int(scale * h)
         img = img.resize((new_w, new_h), Image.ANTIALIAS)
-    img_file = BytesIO()
-    img.save(img_file, 'png')
-    img_file_size = img_file.tell()
-    if img_file_size > FILE_SIZE_THRESHOLD:
-        img.save(file_path, optimize=True, quality=95)
+
+    img = img.convert('RGB')
+    img_file_compressed = BytesIO()
+    if byte_image > FILE_SIZE_THRESHOLD:
+        img.save(img_file_compressed, 'jpeg', optimize=True, quality=95)
     else:
-        img.save(file_path, optimize=False, quality=100)
-    return filename
+        img.save(img_file_compressed, 'jpeg', optimize=False, quality=100)
+    output_size = img_file_compressed.tell()
+    print("Image compressed remain {}%: {} bytes -> {} bytes".format(round(output_size * 100 / byte_image, 1),
+                                                                     byte_image,output_size))
+    return img_file_compressed
--- run.py
+++ run.py
@@ -1,41 +1,53 @@
-from flask import Flask, url_for, request, send_from_directory, g, jsonify
-from compress_image import compress_images, BASE_FOLDER
-from utils import get_images
 import logging
+
+from flask import Flask, request, send_from_directory, jsonify, send_file
+from flask_restful import reqparse, Resource, Api
+from werkzeug.datastructures import FileStorage
+from compress_image import compress_image, BASE_FOLDER
+from utils import get_image, ImageException
 
 LOGGER = logging.getLogger()
 
 flask_app = Flask(__name__, static_folder='web/base/static')
+api = Api(flask_app)
 
 
-@flask_app.route("/compress_image", methods=["POST"])
-def compress():
-    try:
-        if request.method == 'POST':
-            params = request.get_json()
-            imgs = get_images(params['image'])
-            img_names = compress_images(imgs)
-            return jsonify({
-                "status_code": 200,
-                "message": "Image has saved.",
-                "image_name": img_names
-            })
-        else:
+class CompressImageApi(Resource):
+    def post(self):
+        try:
+            parser = reqparse.RequestParser()
+            parser.add_argument("image", required=True, location=['form', 'args', 'json', 'files'])
+            args = parser.parse_args(strict=True)
+            if "FileStorage" in args.image:
+                parser.replace_argument('image', type=FileStorage, required=True, location='files')
+            args = parser.parse_args(strict=True)
+            img, byte_image = get_image(args.image)
+            img_compressed = compress_image(img, byte_image)
+            img_compressed.seek(0)
+            return send_file(
+                img_compressed,
+                mimetype='image/jpeg',
+                as_attachment=True,
+                attachment_filename="compressed_image.jpg"
+            )
+        except ImageException as e:
             return {
                 "status_code": 400,
-                "message": "Please sent request with GET method"
+                "message": str(e)
             }
-    except Exception as e:
-        return jsonify({
-            "status_code": 500,
-            "message": "Internal Server Error."
-        })
+        except Exception as e:
+            return {
+                "status_code": 500,
+                "message": "Internal Server Error."
+            }
 
 
-@flask_app.route("/get_image/<filename>")
-def get_image(filename):
-    return send_from_directory(BASE_FOLDER, filename, as_attachment=True)
+# @flask_app.route("/get_image/<filename>")
+# def get_image(filename):
+#     return send_from_directory(BASE_FOLDER, filename, as_attachment=True)
 
+
+api.add_resource(CompressImageApi, '/compress_image')
 
 if __name__ == '__main__':
     flask_app.run("0.0.0.0", debug=True)
