7ec1477cac4a3ca08e9757ab49da1b9a517f5807
thiennt <tienthienhd@gmail.com>
2020-11-12 17:59:02 +0700

    add requirement

--- run.py
+++ run.py
@@ -47,7 +47,7 @@ class CompressImageApi(Resource):
 #     return send_from_directory(BASE_FOLDER, filename, as_attachment=True)
 
 
-api.add_resource(CompressImageApi, '/compress_image')
+api.add_resource(CompressImageApi, '/compress-image')
 
 if __name__ == '__main__':
-    flask_app.run("0.0.0.0", debug=True)
+    flask_app.run("0.0.0.0", port=15000, debug=True)
