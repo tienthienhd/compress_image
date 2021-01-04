52f3b1d70e6b7f8926ff4fb795d93088cf0cae6c
thiennt <tienthienhd@gmail.com>
2021-01-04 11:13:51 +0700

    option resize image

---
+++ README.md
@@ -0,0 +1,8 @@
+
+
+# Distribution
+```script
+python setup.py sdist bdist_wheel
+
+twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+```     
--- compress_image/main.py
+++ compress_image/main.py
@@ -6,15 +6,16 @@ from PIL import Image
 BASE_DIM = 2000
 
 
-def compress_image(input_path: str, output_path: str, quality: int = 95) -> str:
+def compress_image(input_path: str, output_path: str, quality: int = 95, resize: int = 0) -> str:
     img = Image.open(input_path)
 
-    w, h = img.size
-    max_dim = max(w, h)
-    if max_dim > BASE_DIM:
-        scale = BASE_DIM / max_dim
-        new_w, new_h = int(scale * w), int(scale * h)
-        img = img.resize((new_w, new_h), Image.ANTIALIAS)
+    if resize == 1:
+        w, h = img.size
+        max_dim = max(w, h)
+        if max_dim > BASE_DIM:
+            scale = BASE_DIM / max_dim
+            new_w, new_h = int(scale * w), int(scale * h)
+            img = img.resize((new_w, new_h), Image.ANTIALIAS)
     img = img.convert('RGB')
     if 20 < quality < 100:
         pass
@@ -30,6 +31,8 @@ def main():
     parser.add_argument("--output", "-o", help="Đường dẫn lưu ảnh đầu ra.")
     parser.add_argument("--quality", "-q", required=False, default=95, type=int,
                         help="Chất lượng ảnh đầu ra. Gía trị int trong khoảng 20 - 100", )
+    parser.add_argument("--resize", "-r", required=False, default=0, type=int,
+                        help="Thay đổi kích thước ảnh. Mặc định là không thay đổi kích thước ảnh. Thay đổi kích thước ảnh nếu reszie=1", )
     args = parser.parse_args()
 
     file_path = args.input
@@ -41,4 +44,4 @@ def main():
     if output_path is None:
         output_path = os.path.join(input_dir, file_id + "_compressed.jpg")
 
-    return compress_image(file_path, output_path, args.quality)
+    return compress_image(file_path, output_path, args.quality, args.resize)
